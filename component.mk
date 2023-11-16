1060d3d91a39fbca6c8055228c93a4e6aa3d25f2
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2023-11-16 13:55:42 +1300

    Fix more

--- component.mk
+++ component.mk
@@ -3,7 +3,7 @@
 #
 # (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
 
-ifdef CONFIG_ESP32_S3_BOX_LITE_BOARD
+ifdef CONFIG_ESP32_S3_BOX_3_BOARD
 COMPONENT_ADD_INCLUDEDIRS += ./esp32_s3_box_3
 COMPONENT_SRCDIRS += ./esp32_s3_box_3
 endif
